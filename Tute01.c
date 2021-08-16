d64aeac8bf989f55cc62f132f12eeef8fdfbc46a
it20620202 <89015836+IT20620202@users.noreply.github.com>
2021-08-16 10:42:46 +0000

    1st one only

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,19 @@
 #include <stdio.h>
 
 int main() {
-  
+  int sub1;
+  int sub2;
+  int avg=0;
+
+  printf("Enter the subject 1 marks -");
+  scanf("%d",&sub1);
+
+  printf("Enter the subject 2 marks -");
+  scanf("%d",&sub2);
+
+  avg=(sub1+sub2)/2;
+
+  printf("The avarage is -%d",avg);
   return 0;
 }
 
